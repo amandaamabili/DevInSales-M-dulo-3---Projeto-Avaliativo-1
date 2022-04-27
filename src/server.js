ff5e9427f08d239131bda1a2bb558c8326bd8021
amandaamabili <amanda_amabilly.b@hotmail.com>
2022-04-26 23:47:08 -0300

    Refact Logger

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
 
-const morgan = require('morgan');
+const morganMiddleware = require("./middlewares/morgan.middleware");
+
 const express = require('express')
 
-const logger = require('./config/logger');
+const logger = require('./config/logger.js');
 const routes = require('./routes')
 require('./database')
 
@@ -13,9 +14,13 @@ const PORT = process.env.PORT || 3333
 const swaggerUI = require('swagger-ui-express')
 const swaggerFile = require('./swagger.json')
 
-app.use(require("morgan")("combined", { "stream": logger.stream }));
+
+app.use(morganMiddleware);
 app.use(express.json())
 app.use(routes)
 app.use('/api/v1/docs', swaggerUI.serve, swaggerUI.setup(swaggerFile))
 
-app.listen(PORT, () => console.log(`Executando na porta ${PORT}`)) 
+app.listen(PORT, () => {
+    logger.info('Server is running on port 9000');
+    console.log(`Executando na porta ${PORT}`)
+}) 
